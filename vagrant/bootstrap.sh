a0ae16dfe400fc0665ecc77bcaf83c5ed38635de
Ömer ÜCEL <omerucel@gmail.com>
2013-12-16 22:51:46 +0200

    Update bootstrap.sh

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -2,6 +2,7 @@
 
 set -u
 export SHELL_SCRIPT_MODULE_PATH="/vagrant/vagrant/modules"
+export APACHE_VHOST_FILE="/vagrant/vagrant/files/vhost.conf"
 source "${SHELL_SCRIPT_MODULE_PATH}/lib.sh"
 
 runModules "base" "mysql" "apache2" "php5" "php5-curl" "php5-gd" "php5-mcrypt" "php5-mysql" "php5-xdebug"
